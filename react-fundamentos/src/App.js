8b8efb63c992c5cba81252d6d1a4b69c10e441ca
Agustin Cantero <cantero.agustin@yahoo.com.ar>
2021-01-18 20:37:47 -0300

    avance

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import TarjetaFruta from './componentes/TarjetaFruta';
 
+const styles = {
+    height: '200px',
+    background: 'gold',
+    padding: '1em',
+    boxSizing: 'border-box'
+}
+
+const Gato = (props) => (
+    <div>
+        <h1>Gato 🐅</h1>
+        <pre>
+            {JSON.stringify(props, null, 4)}
+        </pre>
+    </div>
+)
+
 class Contador extends Component {
     state = {
         clicks: 0,
@@ -33,13 +49,57 @@ class Contador extends Component {
     }
 }
 
-const App = () => (
-    <div>
-        <Contador></Contador>
-        {/* <TarjetaFruta name="Sandia" price={3.00} />
-        <TarjetaFruta name="Naranja" price={2.00} />
-        <TarjetaFruta name="Kiwi" price={4.00} /> */}
-    </div>
-)
+class App extends Component {
+
+    // state = {
+    //     fuerza: 100,
+    //     vidasRestantes: 7
+    // }
+
+    state = {
+        x: 0,
+        y: 0
+    }
+
+    manejador = (event) => {
+        this.setState({
+            x: event.clientX,
+            y: event.clientY
+        })
+    }
+
+    render() {
+        const otrosDatos = {
+            raza: 'tropical',
+            peleasNocturnas: 300
+        }
+
+        return (
+            <div>
+                <div
+                    style={styles}
+                    onMouseMove={this.manejador}>
+                    <div>
+                        x: {this.state.x}
+                    </div>
+                    <div>
+                        y: {this.state.y}
+                    </div>
+                </div>
+                <Gato
+                    name='Garfield'
+                    age='2 anos'
+                    {...otrosDatos}
+                // {...this.state} 
+                />
+                <Contador />
+                <TarjetaFruta name="Sandia" price={3.00} />
+                <TarjetaFruta name="Naranja" price={2.00} />
+                <TarjetaFruta name="Kiwi" price={4.00} />
+            </div>
+        )
+    }
+}
+
 
 export default App
